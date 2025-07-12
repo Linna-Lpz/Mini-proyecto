864c671bab9fac9013bc6ee1926a81001615c90b
Lina-Lpz <catalina.lopez.a@usach.cl>
2025-07-12 18:42:00 -0400

    Modificación de estructuras
    comment-article
    creación de users

--- go-template-structured/models/comment.go
+++ go-template-structured/models/comment.go
@@ -1,12 +1,16 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // Comment: Comentario de un usuario acerca de un artículo
 type Comment struct {
-    ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-    Author      string              `json:"author" bson:"author"`
-    Text        string              `json:"text" bson:"text"`
-    CreatedAt   string              `json:"created_at" bson:"created_at"`
-    ArticleID   string              `json:"article_id" bson:"article_id"`
+    ID          primitive.ObjectID      `json:"id" bson:"_id,omitempty"`
+    ArticleID   primitive.ObjectID      `json:"article_id" bson:"article_id"`
+    AuthorID    primitive.ObjectID      `json:"author_id" bson:"author_id"`
+    Text        string                  `json:"text" bson:"text"`
+    CreatedAt   time.Time               `json:"created_at" bson:"created_at"`
 }
